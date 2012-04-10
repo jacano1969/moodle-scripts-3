bfd8055a74a5e4c25b4d2459e5db2cd6d44127d0
Jason Fowler <phalacee@gmail.com>
2012-04-10 14:17:16 +0800

    preparing for the inclusion of update.php and making reset.php less linux centric

--- config.php
+++ config.php
@@ -4,7 +4,22 @@ $pass = "Moodle#2012";
 
 //					0				1					2				3	4		5	6	 7
 //		   "version"   	array("moodledir", 			"wwwroot", 				"datadir", 		       "dbtype", "dbhost",    "dbuser", "dbpass", "dbname"),
-$instances = array("19" => 	array("/www/repos/stable19/moodle/", 	"http://jason.moodle.local/stable19", 	"/www/repos/stable19/data/",  "mysqli", "localhost", "root",   "moodle", "stable19"),
+$reset     = array("i19" => 	array("/www/repos/istable19/moodle/", 	"http://jason.moodle.local/istable19", 	"/www/repos/istable19/data/",  "mysqli", "localhost", "root",   "moodle", "istable19"),
+                   "i20" => 	array("/www/repos/istable20/moodle/", 	"http://jason.moodle.local/istable20", 	"/www/repos/istable20/data/",  "mysqli", "localhost", "root",   "moodle", "istable20"),
+                   "i21" => 	array("/www/repos/istable21/moodle/", 	"http://jason.moodle.local/istable21", 	"/www/repos/istable21/data/",  "mysqli", "localhost", "root",   "moodle", "istable21"),
+                   "i22" => 	array("/www/repos/istable22/moodle/", 	"http://jason.moodle.local/istable22", 	"/www/repos/istable22/data/",  "mysqli", "localhost", "root",   "moodle", "istable22"),
+                   "imaster" => array("/www/repos/imaster/moodle/", 	"http://jason.moodle.local/imaster", 	"/www/repos/imaster/data/",    "mysqli", "localhost", "root",   "moodle", "imaster"),
+
+		   "ialt" =>	array("/www/repos/altmaster/moodle/", 	"http://jason.moodle.local/altmaster", 	"/www/repos/altmaster/data/",  "mysqli", "localhost", "root",   "moodle", "altmaster"),
+
+		   "ip20" => 	array("/www/repos/ipStable20/moodle/", 	"http://jason.moodle.local/ipstable20",	"/www/repos/ipStable22/data/", "pgsql",  "localhost", "root",   "moodle", "ipstable20"),
+		   "ip21" => 	array("/www/repos/ipStable21/moodle/", 	"http://jason.moodle.local/ipstable21",	"/www/repos/ipStable22/data/", "pgsql",  "localhost", "root",   "moodle", "ipstable21"),
+		   "ip22" => 	array("/www/repos/ipStable22/moodle/", 	"http://jason.moodle.local/ipstable22",	"/www/repos/ipStable22/data/", "pgsql",  "localhost", "root",   "moodle", "ipstable22"),
+		   "ipmaster" => array("/www/repos/ipMaster/moodle/", 	"http://jason.moodle.local/ipmaster",	"/www/repos/ipMaster/data/",   "pgsql",  "localhost", "root",   "moodle", "ipmaster"),
+	 	  );
+
+
+$update    = array("19" => 	array("/www/repos/stable19/moodle/", 	"http://jason.moodle.local/stable19", 	"/www/repos/stable19/data/",  "mysqli", "localhost", "root",   "moodle", "stable19"),
                    "20" => 	array("/www/repos/stable20/moodle/", 	"http://jason.moodle.local/stable20", 	"/www/repos/stable20/data/",  "mysqli", "localhost", "root",   "moodle", "stable20"),
                    "21" => 	array("/www/repos/stable21/moodle/", 	"http://jason.moodle.local/stable21", 	"/www/repos/stable21/data/",  "mysqli", "localhost", "root",   "moodle", "stable21"),
                    "22" => 	array("/www/repos/stable22/moodle/", 	"http://jason.moodle.local/stable22", 	"/www/repos/stable22/data/",  "mysqli", "localhost", "root",   "moodle", "stable22"),
--- reset.php
+++ reset.php
@@ -1,6 +1,6 @@
 <?php
 require_once("config.php");
-foreach ($instances as $version => $instance) {
+foreach ($reset as $version => $instance) {
 	if ($instance[3] == "mysqli") {
 		$db = mysql_connect($instance[4], $instance[5], $instance[6]);
 		mysql_query("DROP DATABASE ". $instance[7]);
@@ -9,7 +9,7 @@ foreach ($instances as $version => $instance) {
 		pg_query('DROP DATABASE '. $instance[7]);
 		pg_query('CREATE DATABASE '. $instance[7] .' OWNER '. $instance[5]);
 	}
-	system('rm '. $instance[0] .'config.php');
+	unlink( $instance[0] .'config.php');
 	system('php '. $instance[0] .'admin/cli/install.php --agree-license --non-interactive --allow-unstable --wwwroot='. $instance[1] .' --dataroot='. $instance[2] .' --dbtype='. $instance[3] .' --dbhost='. $instance[4] .' --dbuser='. $instance[5] .' --dbpass='. $instance[6] .' --dbname='. $instance[7] .' --fullname='. $instance[7] .' --shortname='. $instance[7] .' --adminuser='. $user .' --adminpass='. $pass);
 	system('chown www-data:www-data '. $instance[0] .'config.php');
 }
