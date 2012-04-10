d4607d4958b7fd48672f7322f5d1de6cd2fc7112
Jason Fowler <phalacee@gmail.com>
2012-04-10 15:28:46 +0800

    fixes to update.php to make it easier to read

--- update.php
+++ update.php
@@ -1,32 +1,24 @@
 <?php
 require_once("config.php");
 foreach ($update as $version => $instance) {
-	
-	system('cd '. $instance[0]);
-echo 'cd '. $instance[0];
-
-//	system('mv config.php ../');
-//	system('git checkout *');
-//	system('mv ../config.php ./');
-//	system('git fetch --all --prune');
-//	system('git checkout '. $instance[8]);
-//	system('git pull');
-//	system('git push github refs/remotes/origin/'. $instance[9]);
-//	system('php admin/cli/upgrade.php --non-interactive --allow-unstable');
-//	system('php admin/cli/cron.php');
-
+	system('cd '. $instance[0] .';
+		mv config.php ../;
+		git checkout *;
+		mv ../config.php ./;
+		git fetch --all --prune;
+		git checkout '. $instance[8] .';
+		git pull;
+		git push github refs/remotes/origin/'. $instance[9] .';
+		php admin/cli/upgrade.php --non-interactive --allow-unstable;
+		php admin/cli/cron.php');
 }
-/*
 foreach ($reset as $version => $instance) {
-	system('cd '. $instance[0]);
-	system('mv config.php ../');
-	system('git checkout *');
-	system('mv ../config.php ./');
-	system('git fetch --all --prune');
-	system('git checkout '. $instance[8]);
-	system('git pull');
-	system('php admin/cli/upgrade.php --non-interactive --allow-unstable');
-	system('php admin/cli/cron.php');
-
+	system('cd '. $instance[0] .';
+		mv config.php ../;
+		git checkout *;
+		mv ../config.php ./;
+		git fetch --all --prune;
+		git pull;
+		php admin/cli/upgrade.php --non-interactive --allow-unstable;
+		php admin/cli/cron.php');
 }
-*/
